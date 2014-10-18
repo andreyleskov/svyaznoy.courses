72ccceb659704c7ad6ef520876418a165c3366ef
aleskov <andrey.leskov@gmail.cpm>
2014-10-18 22:37:50 +0400

    singleton example

---
+++ DesignPatterns/Examples/Singleton/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Привет, " + Settings.Instance);
+            Console.WriteLine("Ты точно один?, " + Settings.Instance);
+            Console.ReadKey();
+        }
+    }
+
+
+    class Settings
+    {
+        public static readonly Settings Instance = new Settings();
+        private Settings()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "Singleton hash: " + this.GetHashCode();
+        }
+    }
+}
