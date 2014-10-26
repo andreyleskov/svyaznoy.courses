8934921c5ca9966daeb780c870284b7050b7a984
aleskov <andrey.leskov@gmail.cpm>
2014-10-26 20:06:43 +0400

    Примеры для ссылочных и значимых типов, ref и out параметров

---
+++ Intruduction in .Net/Lesson 2/Examples/ReferenceAndValueTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValueTypes
+{
+    class Program
+    {
+        class ValueHolder
+        {
+            public int Value;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+
+            Exchange(ref a,ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            ValueHolder holderA = new ValueHolder();
+            ValueHolder holderB = new ValueHolder();
+            holderA.Value = 10;
+            holderB.Value = 5;
+            Exchange(holderA,holderB);
+            Console.WriteLine(holderA.Value);
+            Console.WriteLine(holderB.Value);
+            Console.ReadKey();
+        }
+
+        static void Exchange(ref int a , ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+        }
+
+        static void Exchange(ValueHolder a, ValueHolder b)
+        {
+            ValueHolder c;
+            c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
---
+++ Intruduction in .Net/Lesson 2/Examples/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    class Program
+    {
+        class ValueHolder
+        {
+            public int Value;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("internal int changed value = " + ChangeValue(a));
+            Console.WriteLine("initial int value  = " + a);
+
+            ValueHolder holder = new ValueHolder();
+            holder.Value = 10;
+
+            Console.WriteLine("internal holder changed value = " + ChangeValue(holder));
+            Console.WriteLine("initial holder value  = " + holder.Value);
+            Console.ReadLine();
+        }
+
+        static int ChangeValue(int value)
+        {
+            value = value + 100;
+            return value;
+        }
+
+        static int ChangeValue(ValueHolder holder)
+        {
+            holder.Value = holder.Value + 100;
+            return holder.Value;
+        }
+    }
+}
